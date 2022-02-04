1424b82cd5f9bffa64929320a25f359e83c34057
Andres Mora <andres@devsenpai.com>
2022-02-04 01:09:55 -0600

    Add prettier

--- packages/components/button/index.tsx
+++ packages/components/button/index.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import { Pressable, NativeSyntheticEvent, NativeTouchEvent, Text } from 'react-native'
+import React from 'react';
+import { Pressable, NativeSyntheticEvent, NativeTouchEvent, Text } from 'react-native';
 
 interface ButtonProps {
-    label: string
-    onPress: (ev: NativeSyntheticEvent<NativeTouchEvent>) => void
+  label: string;
+  onPress: (ev: NativeSyntheticEvent<NativeTouchEvent>) => void;
 }
 
-export const Button = ( {label, onPress} : ButtonProps) => {
-    return <Pressable onPress={onPress}>
-        <Text>
-            {label}
-        </Text>
+export const Button = ({ label, onPress }: ButtonProps) => {
+  return (
+    <Pressable onPress={onPress}>
+      <Text>{label}</Text>
     </Pressable>
-}
+  );
+};
