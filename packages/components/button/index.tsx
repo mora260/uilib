ed68d95e157f70d8d5dc484311f7d868401f321b
Andres Mora <andres@devsenpai.com>
2022-02-04 14:16:28 -0600

    Add styles and change entrypoint

--- packages/components/button/index.tsx
+++ packages/components/button/index.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
-import { Pressable, NativeSyntheticEvent, NativeTouchEvent, Text } from 'react-native';
+import { NativeSyntheticEvent, NativeTouchEvent } from 'react-native';
+import { Pressable, Text } from 'dripsy';
 
 interface ButtonProps {
   label: string;
+  color?: string;
   onPress: (ev: NativeSyntheticEvent<NativeTouchEvent>) => void;
 }
 
-export const Button = ({ label, onPress }: ButtonProps) => {
+export const Button = ({ label, color = 'royalblue', onPress }: ButtonProps) => {
+  const styles = {
+    buttonStyles: {
+      backgroundColor: color,
+      padding: 10,
+      margin: 12,
+      borderRadius: 6,
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      minHeight: 50,
+    },
+  };
+
   return (
-    <Pressable onPress={onPress}>
+    <Pressable onPress={onPress} sx={styles.buttonStyles}>
       <Text>{label}</Text>
     </Pressable>
   );
